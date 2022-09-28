140ba662a3927dda700e23165676b413f31e2944
kazuikios <slash0juice@gmail.com>
2022-09-29 00:23:06 +0900

    feat: add mode.

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,23 @@ class HitAndBlow {
     private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     private answer: string[] = []
     private tryCount = 0
+    private mode: 'normal' | 'hard'
+
+    constructor(mode: 'normal' | 'hard') {
+        this.mode = mode
+    }
+
+    private getAnswerLength(): number {
+        switch (this.mode) {
+            case "normal":
+                return 3
+            case "hard":
+                return 4
+        }
+    }
 
     setting() {
-        const answerLength = 3
+        const answerLength = this.getAnswerLength()
         while (this.answer.length < answerLength) {
             const randNum = Math.floor(Math.random() * this.answerSource.length)
             const selectedItem = this.answerSource[randNum]
@@ -26,7 +40,8 @@ class HitAndBlow {
     }
 
     async play() {
-        const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+        const answerLength = this.getAnswerLength()
+        const inputArr = (await promptInput(`「,」区切りで${answerLength}つの数字を入力してください`)).split(',')
 
         if (!this.validate(inputArr)) {
             printLine('無効な入力です')
@@ -75,7 +90,7 @@ class HitAndBlow {
 }
 
 ;(async () => {
-    const hitAndBlow = new HitAndBlow()
+    const hitAndBlow = new HitAndBlow("hard")
     hitAndBlow.setting()
     await hitAndBlow.play()
     hitAndBlow.end()
